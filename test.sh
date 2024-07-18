7686f0589c9e26046952bb9a4d4958a5deaa3f8c
Bash <>
2024-07-18 16:15:49 +0800

    test

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+export RED_COLOR='\e[1;31m'
+export GREEN_COLOR='\e[1;32m'
+export YELLOW_COLOR='\e[1;33m'
+export BLUE_COLOR='\e[1;34m'
+export PINK_COLOR='\e[1;35m'
+export SHAN='\e[1;33;5m'
+export RES='\e[0m'
+
+[ -n "$DEVTYPE" ] && echo -e "${GREEN_COLOR}Device Type: $DEVTYPE${RES}" || echo -e "${GREEN_COLOR}Device Type: Default${RES}"
